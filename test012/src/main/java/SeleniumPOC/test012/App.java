5ce1ddb0f22dc521cba90d652e5c5de3447e62ba
Shravan <Shravan@Shravan-PC>
2020-04-24 16:11:34 +0530

    aAAAA

---
+++ test012/src/main/java/SeleniumPOC/test012/App.java
@@ -0,0 +1,13 @@
+package SeleniumPOC.test012;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
